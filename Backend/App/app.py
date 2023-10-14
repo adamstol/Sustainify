4280bd6fb378efb0e1c01bf158d2d8aaf5c06001
Zhenxu Wang <mason123wp@gmail.com>
2023-10-14 11:54:01 -0400

    add /result

--- Backend/App/app.py
+++ Backend/App/app.py
@@ -39,5 +39,28 @@ def ask():
     return jsonify({'answer': assistant_message}), 200
 
 
+@app.route('/result', methods=['POST'])
+def result():
+    data = request.get_json()
+
+    if not data or 'message' not in data:
+        return jsonify({'error': 'Message is required'}), 400
+
+    message = data['message']
+
+    # Make a request to OpenAI's v1/chat/completions endpoint
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": message}
+        ]
+    )
+
+    # Extract the assistant's message from the response
+    assistant_message = response['choices'][0]['message']['content']
+
+    return jsonify({'answer': assistant_message}), 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
